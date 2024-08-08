3f5ed4306a8fff7dffcdd23ab7148fd9836018a7
elliottsaruski <elliottxsaruski@gmail.com>
2024-08-07 22:36:54 -0400

    second commit - content

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import sonlogo from "../src/assets/SON_LOGO_darkmode.png";
+
+function App() {
+  return (
+    <>
+      <img src={sonlogo} alt="" style={{ height: "15rem" }} />
+      <h2>Congrats!</h2>
+      <span>
+        you scanned the QR code ~ u win! <br />
+        now come on august 16th
+      </span>
+    </>
+  );
+}
+
+export default App;
+
+//doyouhavesymptoms.netlify.app
